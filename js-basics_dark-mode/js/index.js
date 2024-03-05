37d2100ca38d9db0ca7b325c0121811bc440f5fd
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-03-05 13:55:06 +0100

    add first js code

---
+++ js-basics_dark-mode/js/index.js
@@ -0,0 +1,13 @@
+console.clear();
+
+const bodyElement = document.querySelector('[data-js="body"]');
+
+bodyElement.classList.add("body.dark");
+
+
+
+data-js="dark-mode-button"
+data-js="light-mode-button"
+data-js="toggle-button"
+
+
