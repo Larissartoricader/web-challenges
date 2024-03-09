f8340ea7172b16dc56852bb082fe0328a008f2c8
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-03-09 23:51:13 +0100

    add pizza challenge solution

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -11,62 +11,55 @@ const output = document.querySelector('[data-js="output"]');
 function calculatePizzaGain(diameter1, diameter2) {
   area1 = Math.PI * (diameter1 / 2) ** 2;
   area2 = Math.PI * (diameter2 / 2) ** 2;
-  console.log("The areaa of Pizza1 is " + area1);
-  console.log("The areaa of Pizza2 is " + area2);
-  const pizzaGain = Math.round(((area2 - area1) / area1) * 100);
-  console.log(
-    "The rounded difference in the areas of the pizzas is " + pizzaGain + "%"
-  );
+  console.log("This is area1 " + area1);
+  console.log("This is area2 " + area2);
+  const pizzaGain = ((area2 - area1) / area1) * 100;
+  console.log("This is the pizza gain in percente " + pizzaGain + "%");
   return pizzaGain;
 }
 
+function updatePizzaDisplay(pizzaElement, newSize) {
+  newDisplaySize = (newSize / 24) * 100;
+  pizzaElement.style.width = newDisplaySize + "px";
+}
+
+function updateOutputColor(size1, size2) {
+  if (size1 < size2) {
+    outputSection.style.backgroundColor = "var(--green)";
+  } else {
+    outputSection.style.backgroundColor = "var(--red)";
+  }
+}
+
 pizzaInput1.addEventListener("input", () => {
-  const pizzaSize1 = pizzaInput1.value;
-  console.log(pizzaSize1);
+  let pizzaSize1 = pizzaInput1.value;
+  console.log("Size of Pizza 1 is " + pizzaSize1);
+  let pizzaSize2 = pizzaInput2.value;
+  console.log("Size of Pizza 2 is " + pizzaSize2);
+
+  let finalPizzaGain = calculatePizzaGain(pizzaSize1, pizzaSize2);
+  let roundedPizzaGain = Math.round(finalPizzaGain);
+  output.textContent = roundedPizzaGain;
+
+  updatePizzaDisplay(pizza1, pizzaSize1);
+
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  const pizzaSize2 = pizzaInput2.value;
-  console.log(pizzaSize2);
+  let pizzaSize1 = pizzaInput1.value;
+  console.log("Size of Pizza 1 is " + pizzaSize1);
+  let pizzaSize2 = pizzaInput2.value;
+  console.log("Size of Pizza 2 is " + pizzaSize2);
+
+  let finalPizzaGain = calculatePizzaGain(pizzaSize1, pizzaSize2);
+  let roundedPizzaGain = Math.round(finalPizzaGain);
+  output.textContent = roundedPizzaGain;
+
+  updatePizzaDisplay(pizza2, pizzaSize2);
+
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
-
-calculatePizzaGain(pizzaSize1, pizzaSize2);
-// --------
-
-// let diameter1 = pizzaInput1.value;
-// console.log(diameter1);
-
-// let diameter2 = pizzaInput2.value;
-// console.log(diameter2);
-
-// // Task 1
-// // define the function calculatePizzaGain here
-
-// function calculatePizzaGain() {
-//   area1 = Math.PI * (diameter1 / 2) ** 2;
-//   area2 = Math.PI * (diameter2 / 2) ** 2;
-//   console.log("The areaa of Pizza1 is " + area1);
-//   console.log("The areaa of Pizza2 is " + area2);
-//   const pizzaGain = Math.round(((area2 - area1) / area1) * 100);
-//   console.log(
-//     "The rounded difference in the areas of the pizzas is " + pizzaGain + "%"
-//   );
-//   return pizzaGain;
-// }
-// calculatePizzaGain();
-
-// let pizzaSize1 = Math.round(area1);
-// let pizzaSize2 = Math.round(area2);
-
-// console.log("The Area of Pizza 1 is " + pizzaSize1 + "cm");
-// console.log("The Area of Pizza 2 is " + pizzaSize2 + "cm");
-
-// pizzaInput1.addEventListener("input", () => {
-//   pizzaInput.value;
-//   console.log(pizzaInput1);
-// });
-
-// pizzaInput2.addEventListener("input", () => {});
 
 // Task 2
 // define the function updatePizzaDisplay here
