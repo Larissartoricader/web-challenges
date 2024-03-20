d911badd735d3958525f9585b144852ffd683c24
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-03-20 10:37:37 +0100

    add solution try catch error display

---
+++ js-error-handling_calculator/js/index.js
@@ -0,0 +1,40 @@
+console.clear();
+
+const form = document.querySelector("form");
+const output = form.querySelector("output");
+const error = document.querySelector("p");
+console.log(error);
+
+const operations = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero!");
+    }
+    return a / b;
+  },
+};
+
+// try {
+//   const result = divide(a, b);
+//   console.log(`${a} times ${b} equals to ${result}`);
+// } catch (error) {
+//   console.log("Please pass a number rather than 0 as divisor, thank you");
+//   error.innerText = "STOP! Pick a better number than ZERO to divide, Dumbass";
+// }
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const firstNumber = Number(event.target.firstNumber.value);
+  const secondNumber = Number(event.target.secondNumber.value);
+  const operation = event.target.operation.value;
+  try {
+    output.innerText = operations[operation](firstNumber, secondNumber);
+  } catch (error) {
+    console.log("Please pass a number rather than 0 as divisor, thank you");
+    output.innerText =
+      "STOP! Pick a better number than ZERO to divide, Dumbass";
+  }
+});
