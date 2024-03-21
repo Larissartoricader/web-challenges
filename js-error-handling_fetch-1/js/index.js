bbf3f32e6fcc0023f6d829e5d70ba09957b17c69
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-03-21 22:41:11 +0100

    add solution js handling fetch

--- js-error-handling_fetch-1/js/index.js
+++ js-error-handling_fetch-1/js/index.js
@@ -18,6 +18,7 @@
 //     `;
 //   })
 // );
+// -------------------------------------------//-----------------------------------
 
 console.clear();
 
@@ -25,37 +26,75 @@ const userElement = document.querySelector(".user");
 const errorElement = document.querySelector(".error");
 
 async function getUser(url) {
-  userElement.innerHTML = "";
-  errorElement.textContent = "";
-
-  const response = await fetch(url);
-  if (!response.ok) {
-    console.log("Network error");
+  const myResponse = await fetch(url);
+  if (!myResponse.ok) {
+    console.log("Erro 1");
+    throw new Error("Network Error!!!!");
+  }
+  try {
+    const json = await myResponse.json();
+    return json.data;
+  } catch (errorParameter) {
+    console.log("FUCK");
+    errorElement.textContent = "Error: " + errorParameter.mesage;
     return null;
   }
-
-  const json = await response.json();
-  return json.data;
 }
 
 document.querySelectorAll("button[data-url]").forEach((button) =>
   button.addEventListener("click", async (event) => {
     try {
+      errorElement.textContent = "";
       const user = await getUser(event.target.dataset.url);
-      console.log(user);
-      if (!user) {
-        errorElement.textContent = "User not found";
-        return;
-      }
-
       userElement.innerHTML = `
-        <h2>${user.first_name} ${user.last_name}</h2>
-        <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
-        `;
-    } catch (errorElement) {
-      console.info(error);
-      errorElement.textContent = error.message;
-      return;
+    <h2>${user.first_name} ${user.last_name}</h2>
+    <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
+    `;
+    } catch {
+      userElement.innerHTML = "bla bla";
     }
   })
 );
+
+// -------------------------------------------//-----------------------------------
+
+// console.clear();
+
+// const userElement = document.querySelector(".user");
+// const errorElement = document.querySelector(".error");
+
+// async function getUser(url) {
+//   userElement.innerHTML = "";
+//   errorElement.textContent = "";
+
+//   const response = await fetch(url);
+//   if (!response.ok) {
+//     console.log("Network error");
+//     return null;
+//   }
+
+//   const json = await response.json();
+//   return json.data;
+// }
+
+// document.querySelectorAll("button[data-url]").forEach((button) =>
+//   button.addEventListener("click", async (event) => {
+//     try {
+//       const user = await getUser(event.target.dataset.url);
+//       console.log(user);
+//       if (!user) {
+//         errorElement.textContent = "User not found";
+//         return;
+//       }
+
+//       userElement.innerHTML = `
+//         <h2>${user.first_name} ${user.last_name}</h2>
+//         <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
+//         `;
+//     } catch (errorElement) {
+//       console.info(error);
+//       errorElement.textContent = error.message;
+//       return;
+//     }
+//   })
+// );
