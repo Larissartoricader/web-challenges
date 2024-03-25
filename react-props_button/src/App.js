b30aa5899555b161cee843573f9e29a71150e873
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-03-25 16:56:35 +0100

    add part of solution Buttons React Props

--- react-props_button/src/App.js
+++ react-props_button/src/App.js
@@ -2,5 +2,25 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <h1>Buttons</h1>
+      <Button text={"Click Me1"} color={"#D81919"} />
+      <br />
+      <Button text={"Click Me2"} color={"#32CD32"} />
+      <br />
+      <Button text={"Click Me3"} color={"#6495ED"} />
+      <br />
+    </div>
+  );
+}
+
+// Inserir a propriedade Disable
+
+export function Button({ text, color }) {
+  return (
+    <button type="button" style={{ backgroundColor: color }}>
+      {text}
+    </button>
+  );
 }
