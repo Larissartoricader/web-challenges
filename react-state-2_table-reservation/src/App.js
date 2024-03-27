cb619cdfd5a1133b5162809968ce1cad1eb2167a
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-03-27 20:40:08 +0100

    add react state 2 Challenge Counter and Table Reservation

---
+++ react-state-2_table-reservation/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import "./styles.css";
+
+export default function App() {
+  const [people, setPeople] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>Place a Table Reservation</h1>
+      <Counter people={people} setPeople={setPeople} />
+      <p>{`You are going to reserve a table ${people} for people.`}</p>
+    </div>
+  );
+}
