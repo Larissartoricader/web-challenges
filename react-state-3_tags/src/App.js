8ae4c0b591bfba210ae548453f437f7c3fe6cd3c
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-03-28 14:08:19 +0100

    add part of Tag Challenge Solution - React state 3

--- react-state-3_tags/src/App.js
+++ react-state-3_tags/src/App.js
@@ -2,13 +2,22 @@ import { useState } from "react";
 import "./styles.css";
 import Form from "./components/Form/index.js";
 import List from "./components/List/index.js";
+import { uid } from "uid";
 
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleOnAddTag(tag) {
+    setTags([...tags, { ...tag, id: uid() }]);
+  }
+
+  // function handleOnDelete(tag) {
+  //   setTags(tags.filter(tag) => tag)
+  // }
+
   return (
     <main className="app">
-      <Form />
+      <Form onAddTag={handleOnAddTag} />
       <List tags={tags} />
     </main>
   );
