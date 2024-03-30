4ca305566315f5c8577c6add1bb744d1f5885ef5
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-03-30 23:59:55 +0100

    add list of names

--- peopleinspace-reactstyle/src/App.js
+++ peopleinspace-reactstyle/src/App.js
@@ -3,20 +3,43 @@ import "./App.css";
 
 function App() {
   const [peopleNumber, SetPeopleNumber] = useState(0);
+  const [names, setNames] = useState();
+  const [craftISS, setCraftISS] = useState();
 
-  async function fetchDate() {
+  async function fetchData() {
     const response = await fetch("http://api.open-notify.org/astros.json");
     const data = await response.json();
     SetPeopleNumber(data.people.length);
-    console.log(data);
+    setNames(
+      data.people.map((person) => {
+        return <li>{person.name}</li>;
+      })
+    );
+
+    setCraftISS(
+      data.people.map((person) => {
+        return person.craft === "ISS" ? <li>{person.name}</li> : null;
+      })
+    );
+    // console.log(data);
+    // console.log(data.people[3].name);
     return data;
   }
 
-  fetchDate();
+  fetchData();
   return (
     <div>
       <h1>📡 People in Space - React Style 🚀</h1>
       <h2>Current {peopleNumber} people are in space</h2>
+      <h2>Names</h2>
+      <ol setNames={setNames}>{names}</ol>
+      <div className="button-box">
+        <button>All</button>
+        <button onClick={setCraftISS}>ISS</button>
+        <button>Tiangong</button>
+      </div>
+      <h2>Craft Names</h2>
+      <ol>{craftISS}</ol>
     </div>
   );
 }
