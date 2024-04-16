e9719b9aa2a3377cca1d44057a7c4f5810cc4d6b
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-04-16 15:21:50 +0200

    add Backend Basics 1

---
+++ backend-basics_greeting/server.js
@@ -0,0 +1,7 @@
+import { createServer } from "node:http";
+
+export const server = createServer((request, response) => {
+  if (request.url === "/") {
+    response.end("Hello, Larissaaaa");
+  }
+});
